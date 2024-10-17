1e108111d928d9a3e21504d784be6cc974afa5b0
98abdulhai <98abdulhai@gmail.com>
2024-10-17 19:32:13 +0600

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Crud Application</title>
+</head>
+<body>
+    <div class="container mt-5">
+    <h1 class="text-primary mt-3 mb-4 text-center"><b>Crud Application</b></h1>
+    @yield('content')
+
+    </div>
+    
+</body>
+</html>
